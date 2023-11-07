177f5f77b31ea4ac6ecdb8ce1dc0fc8de29fd936
clarke <149320067+clarkechong@users.noreply.github.com>
2023-11-07 09:37:03 +0000

    complete js exercise 08 calculator

--- odin-javascript-exercises/08_calculator/calculator.js
+++ odin-javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return (x+y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return (x-y);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = array.reduce((sum, current) => sum + current);
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array.reduce((sum, current) => sum * current);
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x==0) return 1;
+	if(x!=1){
+    return x*factorial(x-1);
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
